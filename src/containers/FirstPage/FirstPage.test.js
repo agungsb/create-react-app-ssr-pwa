7a67162e2633f37266cd436c32dea218615c17fd
agungsb <agung.surya@live.com>
2017-06-17 06:52:55 +0700

    Add test files

--- src/containers/FirstPage/FirstPage.test.js
+++ src/containers/FirstPage/FirstPage.test.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { FirstPage } from 'containers';
+import FirstPage from 'containers/FirstPage/FirstPage';
+
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+
+import ApiClient from './../../utils/ApiClient'
+import configureStore from './../../redux/create'
+
+// Let the reducers handle initial state
+const initialState = {};
+const client = new ApiClient();
+const store = configureStore(client, initialState);
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<FirstPage />, div)
+  ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <FirstPage />
+    </BrowserRouter>
+  </Provider>, div)
 })
 
